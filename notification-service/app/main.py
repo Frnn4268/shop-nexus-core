058a5cb4b4d8b78350e2a21ffc967bee74de4bed
Frnn4268 <fmartinezm7@miumg.edu.gt>
2025-04-17 23:20:48 -0600

    Adding new environment port variables in Python projects, and adding new .json coll

--- notification-service/app/main.py
+++ notification-service/app/main.py
@@ -9,4 +9,4 @@ app = Flask(__name__)
 Thread(target=start_consumer, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8004)
+    app.run(host='0.0.0.0', port=os.getenv("PORT", 8004))
